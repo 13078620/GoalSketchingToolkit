195687a987ed5ef6de2cffa91db8937e2ce0452f
13078620 <c.p.berryman@btinternet.com>
2015-07-17 23:18:04 +0100

    Goal Sketching Toolkit commit.

--- src/goalsketchingtoolkit/Goal.java
+++ src/goalsketchingtoolkit/Goal.java
@@ -6,465 +6,337 @@
 package goalsketchingtoolkit;
 
 import java.util.ArrayList;
-import java.awt.geom.Point2D;
 
 /**
- * Describes a node on a goal graph which may be a parent, child or leaf node.
  *
- * @author Chris Berryman - Oxford Brookes University - 2015
+ * @author Chris
  */
-public class Goal extends GoalSketchingNode {
+public class Goal extends GSnode {
 
     /**
-     * The goal oriented proposition for this graph node.
+     * Denotes whether this goal is the root goal or not.
      */
-    private Proposition proposition;
-
+    private boolean isRoot;
     /**
-     * The unique id for this graph node.
+     * Denotes whether this goal is entailed or not.
+     */
+    private boolean entailed;
+    /**
+     * Denotes whether this goal is operationalized or not.
+     */
+    private boolean operationalized;
+    /**
+     * Denotes whether this goal has a goal oriented proposition or not.
+     */
+    private boolean hasGop;
+    /**
+     * The ID of this Goal.
      */
     private String id;
-
     /**
-     * The list of children graph nodes for this graph node.
+     * The fit acceptance test of this goal if it has one.
      */
-    private ArrayList<GraphNode> children = new ArrayList<>();
-
+    private String fit;
     /**
-     * The list of operationalizers for this graph node contained in an
-     * operationalizer node.
+     * The children of this goal which can contain either an and 
+     * entailment/an or entailment/operationalizing products/an 
+     * assumption termination and a goal oriented proposition.
      */
-    private OperationalizerNode operationalizerNode;
-
-    private AssumptionTerminationNode assumptionTerminationNode;
-
+    private ArrayList<GSnode> children;
     /**
-     * Specifies whether this graph node is a parent or not.
+     * The graphical properties of this goal.
      */
-    private boolean parent;
+    private GSnodeGraphics graphicalProperties;
 
     /**
-     * Specifies whether this graph node is a child or not.
+     * Constructs a Goal and initialises it's array list of children.
      */
-    private boolean child;
-    
-    private boolean operationalized;
-    
-    private boolean terminated;
-
-    /**
-     * The parent graph node for this graph node if it has one.
-     */    
-    private GraphNode par;
-
-    /**
-     * The description for this graph node if it is a description node.
-     */
-    private String description;
-    //private boolean hasTwin;
-
-    private boolean root;
-
-    /**
-     * Constructs a new graph node with no proposition, description or ID.
-     */
-    public GraphNode() {
-        super();
+    public Goal() {
+        children = new ArrayList();
     }
 
     /**
-     * Constructs a graph node with no specified proposition for the purposes of
-     * describing description nodes.
+     * Constructs a Goal, initialises it's array list of children and sets the
+     * ID, fit acceptance test and graphical properties.
      *
-     * @param x the starting x position of the graph node.
-     * @param y the starting y position of the graph node.
-     * @param width the width of the graph node.
-     * @param height the height of the graph node.
-     * @param description the description of the graph node.
-     * @param id the unique identifier for this graph node.
+     * @param id the ID of this Goal.
+     * @param graphicalProperties the graphical properties of this goal.
      */
-    public GraphNode(double x, double y, int width, int height, String description, String id) {
-
-        super(x, y, width, height);
-        this.description = description;
-        this.parent = true;
+    public Goal(String id, GSnodeGraphics graphicalProperties) {
         this.id = id;
-
+        children = new ArrayList();
     }
 
     /**
-     * Constructs a graph node with a specified proposition, id and boolean
-     * value which denotes if this graph node is a parent or not.
+     * Constructs a Goal, initialises it's array list of children and sets the
+     * ID, fit acceptance test and graphical properties.
      *
-     * @param x the starting x position of the graph node.
-     * @param y the starting y position of the graph node.
-     * @param width the width of the graph node.
-     * @param height the height of the graph node.
-     * @param proposition the proposition of this graph node.
-     * @param parent a true boolean value if this graph node is a parent, false
-     * otherwise.
-     * @param id the unique identifier for this graph node.
+     * @param id the ID of this Goal.
+     * @param gop the goal oriented proposition of this goal.
+     * @param gp the graphical properties of this goal.
      */
-    public GraphNode(double x, double y, int width, int height, Proposition proposition, boolean parent, boolean child, String id) {
-
-        super(x, y, width, height);
-        this.proposition = proposition;
-        this.parent = parent;
-        this.child = child;
+    public Goal(String id, GoalOrientedProposition gop, GSnodeGraphics gp) {
         this.id = id;
-        //this.children = new ArrayList<>();
+        children = new ArrayList();
+        children.add(gop);
+    }
+    
+    /**
+     * Returns a reference to this goal because it is a composite.
+     *
+     * @see GSnode#getCompositeGSnode() getCompositeGSnode.
+     * @return a reference to this goal.
+     */
+    @Override
+    public GSnode getCompositeGSnode() {
+        return this;
+    }
+
+    /**
+     * Adds a child node to this goal.
+     *
+     * @throws IllegalArgumentException()if this goal is already has a goal
+     * oriented proposition, is semantically entailed or operationalized.
+     * @param node the node to add.
+     */
+    @Override
+    public void addChild(GSnode node) {
+
+        if (getClass() == node.getClass()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (entailed || operationalized || hasGop) {
+            throw new IllegalArgumentException();
+        }
+
+        if (node.getClass()
+                .toString().contains("AndEntailment")
+                || node.getClass().toString().contains("OrEntailment")) {
+            entailed = true;
+        }
+
+        if (node.getClass()
+                .toString().contains("OperationalizingProducts")) {
+            operationalized = true;
+        }
+
+        if (node.getClass()
+                .toString().contains("GoalOrientedProposition")) {
+            hasGop = true;
+        }
+
+        children.add(node);
 
     }
 
     /**
-     * Sets the id of this graph node.
+     * Removes a child from a goal.
+     *
+     * @param node the node to remove.
+     */
+    @Override
+    public void removeChild(GSnode node) {
+
+        if (node.getClass()
+                .toString().contains("AndEntailment")
+                || node.getClass().toString().contains("OrEntailment")) {
+            entailed = false;
+        }
+
+        if (node.getClass()
+                .toString().contains("OperationalizingProducts")) {
+            operationalized = false;
+        }
+
+        if (node.getClass()
+                .toString().contains("GoalOrientedProposition")) {
+            hasGop = false;
+        }
+
+        children.remove(node);
+        if (children.isEmpty()) {
+            hasChildren = false;
+        }
+    }
+
+    /**
+     * Returns this goal's parent node.
+     *
+     * @return the parent node of this goal.
+     */
+    @Override
+    public GSnode getParent() {
+        return parent;
+    }
+
+    /**
+     * Sets this goal's parent.
+     *
+     * @param node the parent Goal Sketching Node of this goal.
+     */
+    @Override
+    public void setParent(GSnode node) {
+        parent = node;
+        hasParent = true;
+    }
+
+    /**
+     * Returns this goal's children.
+     *
+     * @return this goal's children.
+     */
+    @Override
+    public ArrayList<GSnode> getChildren() {
+        return children;
+    }
+
+    /**
+     * Sets this goal's children.
+     *
+     * @param children the children of this goal.
+     */
+    @Override
+    public void setChildren(ArrayList<GSnode> children) {
+        this.children = children;
+    }
+
+    /**
+     * Returns a boolean to denote whether goal is a parent or not.
+     *
+     * @return true if this goal is a parent, false otherwise.
+     */
+    @Override
+    public boolean isParent() {
+        return hasChildren;
+    }
+
+    /**
+     * Returns a boolean to denote whether this goal is a child or not.
+     *
+     * @return true if this goal is a child, false otherwise.
+     */
+    @Override
+    public boolean isChild() {
+        return hasParent;
+    }
+
+    /**
+     * Sets this Goal as the root node with a true boolean value if that is the
+     * case.
+     *
+     * @param isRoot the boolean value which denote if this Goal is the root
+     * node or not.
+     */
+    public void setIsRootNode(boolean isRoot) {
+        this.isRoot = isRoot;
+    }
+
+    /**
+     * Returns a boolean to denote whether this Goal is the root node or not.
+     *
+     * @return true if this Goal is the root node, false otherwise.
+     */
+    public boolean isRootNode() {
+        return isRoot;
+    }
+
+    /**
+     * Returns this goal's goal oriented proposition.
+     *
+     * @throws NullPointerException() if the goal does not have a
+     * @return this goal's goal oriented proposition.
+     */
+    public GoalOrientedProposition getProposition() {
+
+        GoalOrientedProposition gop = null;
+        for (Object o : children) {
+            if (o.getClass() == gop.getClass()) {
+                gop = (GoalOrientedProposition) o;
+            }
+        }
+        return gop;
+    }
+
+    /**
+     * Sets the id of this goal.
      *
      * @param id the id.
      */
     public void setID(String id) {
-
         this.id = id;
-
     }
 
     /**
-     * Returns the id of this graph node.
+     * Returns the id of this goal.
      *
      * @return the id.
      */
     public String getId() {
-
         return id;
+    }
+
+    /**
+     * Sets the fit acceptance test of this goal.
+     *
+     * @param fit the fit acceptance test.
+     */
+    public void setFit(String fit) {
+
+        this.fit = fit;
 
     }
 
     /**
-     * Sets the goal oriented proposition for this graph node.
+     * Returns the fit acceptance test of this goal.
      *
-     * @param proposition the proposition to specify.
+     * @return the fit acceptance test.
      */
-    public void setProposition(Proposition proposition) {
+    public String getFit() {
 
-        this.proposition = proposition;
+        return fit;
 
     }
 
     /**
-     * Returns the goal oriented proposition for this graph node.
+     * Checks whether two goals are equal.
      *
-     * @return the proposition of this graph node.
-     */
-    public Proposition getProposition() {
-
-        return proposition;
-
-    }
-
-    /**
-     * Appends a child graph node to this graph node and sets a reference to
-     * this graph node as the child's parent.
+     * The result is true if and only if the argument is not null and is a goal
+     * object that has the same ID as this goal.
      *
-     * @param child the child graph node to add to this graph node.
+     * @param otherObject the Object to compare with this goal.
+     * @return true if the objects are equal; false otherwise.
      */
-    public void addChildNode(GraphNode child) {
+    @Override
+    public boolean equals(Object otherObject) {
 
-        child.setParent(this);
-        children.add(child);
-
-    }
-
-    /**
-     * Sets the child graph node list for this graph node.
-     *
-     * @param children the list of children to set.
-     */
-    public void setChildGraphNodes(ArrayList<GraphNode> children) {
-
-        for (GraphNode c : children) {
-            c.setParent(this);
+        if (otherObject == null) {
+            return false;
         }
-        this.children = children;
-
+        if (getClass() != otherObject.getClass()) {
+            return false;
+        }
+        Goal other = (Goal) otherObject;
+        return getId().equals(other.getId());
     }
 
     /**
-     * Returns the list of child graph nodes for this graph node.
+     * Returns this Goal's graphical properties.
      *
-     * @return the list of this graph node's children.
+     * @return the graphical properties of this Goal.
      */
-    public ArrayList getChildNodes() {
-
-        return children;
-
+    @Override
+    public GSnodeGraphics getGraphicalProperties() {
+        return graphicalProperties;
     }
 
     /**
-     * Sets whether this graph node is a parent or not.
+     * Sets this Goal's graphical properties.
      *
-     * @param parent true if this graph node is a parent, false otherwise.
+     * @param graphicalProperties this Goal's graphical properties.
      */
-    public void setIsParent(boolean parent) {
-
-        this.parent = parent;
-
+    @Override
+    public void setGraphicalProperties(GSgraphics graphicalProperties) {
+        this.graphicalProperties = (GSnodeGraphics) graphicalProperties;
     }
-
-    /**
-     * Sets whether this graph node is a child or not.
-     *
-     * @param child true if this graph node is a child, false otherwise.
-     */
-    public void setIsChild(boolean child) {
-
-        this.child = child;
-
-    }
-
-    /**
-     * Sets whether this graph node is operationalized or not.
-     *
-     * @param operationalized true if this graph node operationalized, false
-     * otherwise.
-     */
-    public void setIsOperationalized(boolean operationalized) {
-
-        this.operationalized = operationalized;
-
-    }
-    
-    /**
-     * Gets whether this graph node is operationalized or not.
-     *
-     * @return true if this graph node is operationalized, false otherwise.
-     */
-    public boolean isOperationalized() {
-
-        return operationalized;
-
-    }
-
-    /**
-     * Returns whether this graph node is a parent or not.
-     *
-     * @return true if this graph node is a parent, false otherwise.
-     */
-    public boolean isParent() {
-
-        return parent;
-
-    }
-
-    /**
-     * Returns whether this graph node is a child or not.
-     *
-     * @return true if this graph node is a child, false otherwise.
-     */
-    public boolean isChild() {
-
-        return child;
-
-    }
-
-    /**
-     * Returns whether this graph node is both a parent and child or not.
-     *
-     * @return true if this graph node is a parent and child, false otherwise.
-     */
-    public boolean isParentAndChild() {
-
-        return child && parent;
-
-    }
-
-    /**
-     * Sets the operationalizer node of this graph node.
-     *
-     * @param ozn the operationalizer node.
-     */
-    public void setOperationalizerNode(OperationalizerNode ozn) {
-
-        ozn.setParent(this);
-        this.operationalizerNode = ozn;
-
-    }
-    
-    /**
-     * Returns the operationalizer node of this graph node.
-     *
-     * @return operationalizer node of this graph node.
-     */
-    public OperationalizerNode getOperationalizerNode() {
-
-        return operationalizerNode;
-
-    }
-
-    /**
-     * Sets the Assumption Termination node of this graph node.
-     *
-     * @param ozn the Assumption Termination node.
-     */
-    public void setAssumptionTerminationNode(AssumptionTerminationNode astn) {
-
-        astn.setParent(this);
-        this.assumptionTerminationNode = astn;
-
-    }
-    
-    /**
-     * Returns the Assumption Termination node of this graph node.
-     *
-     * @return Assumption Termination node of this graph node.
-     */
-    public AssumptionTerminationNode getAssumptionTerminationNode() {
-
-        return assumptionTerminationNode;
-
-    }
-
-    public void setIsTerminated(boolean terminated) {
-        
-        this.terminated = terminated;
-        
-    }
-    
-    public boolean isTerminated() {
-        
-        return this.assumptionTerminationNode != null;
-        
-    }
-
-    /**
-     * Appends an operationalizer to the operationalizer node for this graph
-     * node.
-     *
-     * @param operationalizer the operationalizer to add.
-     */
-    public void addOperationalizer(Operationalizer operationalizer) {
-
-        this.operationalizerNode.addOperationalizer(operationalizer);
-
-    }
-
-    /**
-     * Appends an operationalizer with a given sub domain to the operationalizer
-     * node for this graph node.
-     *
-     * @param operationalizer the operationalizer to add.
-     * @param subDomain the sub domain of the operationalizer to add.
-     */
-    public void addOperationalizer(Operationalizer operationalizer, String subDomain) {
-
-        operationalizer.addSubDomain(subDomain);
-        this.operationalizerNode.addOperationalizer(operationalizer);
-
-    }
-
-    /**
-     * Appends an operationalizer with a given list of sub domains to the
-     * operationalizer node for this graph node.
-     *
-     * @param operationalizer the operationalizer to add.
-     * @param subDomains the sub domains of the operationalizer to add.
-     */
-    public void addOperationalizer(Operationalizer operationalizer, ArrayList subDomains) {
-
-        operationalizer.setSubDomains(subDomains);
-        this.operationalizerNode.addOperationalizer(operationalizer);
-
-    }
-
-    /**
-     * Returns whether this graph node is a operationalized or not.
-     *
-     * @return true if this graph node is operationalized, false otherwise.
-     */
-    public boolean isOperationalized2() {
-
-        return this.operationalizerNode != null;
-
-    }
-
-    /**
-     * Sets the reference to this graph node's parent.
-     *
-     * @param parent the specified parent graph node for this graph node.
-     */
-    public void setParent(GraphNode parent) {
-
-        this.par = parent;
-
-    }
-
-    /**
-     * Returns the reference to this graph node's parent.
-     *
-     * @return the parent graph node of this graph node.
-     */
-    public GraphNode getParent() {
-
-        return this.par;
-
-    }
-
-    //getDescription()
-    public double getRefinementBottomX() {
-
-        return getX() + getWidth() / 2;
-
-    }
-
-    public double getRefinementBottomY() {
-
-        return getY() + getHeight() + 100;
-
-    }
-
-    public double getRefinementCentreX() {
-
-        return getX() + getWidth() / 2;
-
-    }
-
-    public double[] getRefinementIntersection(double x1, double y1, double x2, double y2) {
-
-        double off_x = x1 - x2;
-        double off_y = y1 - y2;
-        double R = 10;
-        //double[] intersectionArray;
-
-        double ls = off_x * off_x + off_y * off_y;
-
-        //if (ls < R * R) {
-        //  intersectionArray = new double[0];
-        //return intersectionArray 
-        //  } else {
-        double scale = R / Math.sqrt(ls);
-        double res_x = off_x * scale + x2;
-        double res_y = off_y * scale + y2;
-        double[] intersectionArray = {res_x, res_y};
-        // }
-
-        return intersectionArray;
-
-    }
-
-    public double getRefinementCentreY() {
-
-        return getY() + getHeight() + 90;
-
-    }
-
-    public void setIsRootNode(boolean isRoot) {
-
-        this.root = isRoot;
-
-    }
-
-    public boolean isRootNode() {
-
-        return this.root;
-
-    }
-
 }
